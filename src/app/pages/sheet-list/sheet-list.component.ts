117e5e7d6bcfa6c87da2cb9f8e4d836b7b0849f0
1521681359@qq.com <1521681359@qq.com>
2019-12-04 22:18:59 +0800

    pagesChange

--- src/app/pages/sheet-list/sheet-list.component.ts
+++ src/app/pages/sheet-list/sheet-list.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { SheetParams, SheetService } from 'src/app/services/sheet.service';
 import { ActivatedRoute } from '@angular/router';
 import { SheetList } from 'src/app/services/data-types/common.type';
+import { setSongList, setPlayList, setCurrentIndex } from 'src/app/store/acitions/play.action';
+import { Store } from '@ngrx/store';
+import { AppStoreModule } from 'src/app/store';
+import { shuffle, findIndex } from 'src/app/utils/array';
+import { PlayState } from 'src/app/store/reducers/player.reducer';
 
 @Component({
   selector: 'app-sheet-list',
@@ -10,6 +15,7 @@ import { SheetList } from 'src/app/services/data-types/common.type';
 })
 export class SheetListComponent implements OnInit {
   sheets:SheetList
+  private playState:PlayState;
   listParams:SheetParams={
     cat:'全部',
     order:'hot',
@@ -17,7 +23,7 @@ export class SheetListComponent implements OnInit {
     limit:35
   }
   orderValue:"hot";
-  constructor(private route:ActivatedRoute,private sheetServe:SheetService) 
+  constructor(private route:ActivatedRoute,private sheetServe:SheetService,private store$:Store<AppStoreModule>) 
   {
 this.listParams.cat=this.route.snapshot.queryParamMap.get('cat')||'全部';
 this.getList();
@@ -32,8 +38,31 @@ this.getList();
       }
     );
   }
-  onPlaySheet(){
-
+  onPlaySheet(id: number) {
+    this.sheetServe.playSheet(id).subscribe(list=>{
+      console.log(list);
+      this.store$.dispatch(setSongList({songList:list}));
+      let trueIndex=0;
+      let trueList=list.slice();
+if(this.playState.playMode.type=="random"){
+  trueList=shuffle(list||[]);
+  trueIndex=findIndex(trueList,list[trueIndex]);
+}
+      this.store$.dispatch(setPlayList({playList:trueList}));
+      this.store$.dispatch(setCurrentIndex({currentIndex:trueIndex}));
+    });
+  }
+  onOrderChange(order:'new'|'hot'){
+    console.log('cat:',order);
+    this.listParams.order=order;
+    this.listParams.offset=1;
+    this.getList();
+  }
+ 
+  onPageChange(page:number){
+    console.log(page)
+    this.listParams.offset=page;
+    this.getList();
   }
 
 }
